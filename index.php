50ac2373d9e39f065b62fa627417bcb87f735b4e
Luke Matakitoga <luke.matakitoga@cetsat.com>
2024-08-13 13:07:18 +0100

    creds

--- index.php
+++ index.php
@@ -5,10 +5,14 @@ use Aws\S3\S3Client;
 
 
 $s3Client = new S3Client([
-    'profile' => 'default',
+    'credentials' => [
+        'key' => 'AKIAYS2NTXKO2Y7XFYOQ',
+        'secret' => '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',
+    ],
     'region' => 'us-west-2',
     'version' => '2006-03-01'
 ]);
+
 
 //Listing all S3 Bucket
 $buckets = $s3Client->listBuckets();
